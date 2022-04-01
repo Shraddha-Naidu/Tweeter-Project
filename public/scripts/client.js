fcc825053075fa6db2c25042fbffcad6ad1c4c63
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-04-01 00:29:41 -0600

    Add hover styling - need to link

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,7 +7,32 @@ const renderTweets = function(tweets) {
 
 $document.ready(function () {
 
-  $('.new-tweet form').submit(function(event) {
+  const createTweetElement = function(tweetData) {
+    const $tweet = $('<article>').addClass('tweet');
+    const daysAgo = timeago.format(tweetData["created_at"]);
+
+    const innerHTML = `
+<header>
+    <img src= ${tweetData.user.avatars}>
+    <span>${tweetData.user.name}</span>
+    <span class="handle">${tweetData.user.handle}</span>
+</header>
+<span>${tweetData.content.text}</span>
+<footer>
+  <span>${daysAgo} days ago</span>
+  <span>
+  <i class="fa-solid fa-circle-star"></i>
+  <i class="fa-solid fa-retweet"></i>
+  <i class="fa-solid fa-flag"></i>
+</span>
+</footer>
+    `;
+
+    return $tweet.append(innerHTML);
+  };
+
+
+   $('.new-tweet form').submit(function (event) {
     event.preventDefault();
 
     $.ajax('/tweets', {
@@ -25,27 +50,10 @@ $document.ready(function () {
   
     loadTweets();
   
-  })
+  });
 
-const createTweetElement = function(tweetData) {
-let $tweet = $('<article>').addClass('tweet');
-
-const $header = $('<header>');
-
-const $img = $('<img>').attr({
-  src: tweetData.user.avatars,
-  alt: `${tweetData.user.handle}-avatar`
-});
-
-$header.append($img);
-    $header.append($('<section>').text(tweetData.user.name));
-    $header.append($('<section>').text(tweetData.user.handle));
-
-    $tweet.append($header);
-  }
 });
 
 
 
-
-renderTweets(data);
+//renderTweets(data);
