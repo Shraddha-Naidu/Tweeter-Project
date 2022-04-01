d2b56a773901d3b3a3b2279fa81bafa762d82ae2
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-03-31 23:36:03 -0600

    Event listener added for submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,31 @@
+const renderTweets = function(tweets) {
+
+  for (const tweet of tweets) {
+    $("tweets-container").append(createTweetElement(tweet))
+    }
+  };
+
 $document.ready(function () {
+
+  $('.new-tweet form').submit(function(event) {
+    event.preventDefault();
+
+    $.ajax('/tweets', {
+      data: $(this).serialize(),
+      method: 'POST'
+    });
+
+    const loadTweets = () => {
+      $.ajax('/tweets', {
+        method: 'GET',
+        dataType: 'JSON'
+      })
+        .then(tweets => renderTweets(tweets));
+    };
+  
+    loadTweets();
+  
+  })
 
 const createTweetElement = function(tweetData) {
 let $tweet = $('<article>').addClass('tweet');
@@ -11,53 +38,14 @@ const $img = $('<img>').attr({
 });
 
 $header.append($img);
-    $header.append($('<article>').text(tweetData.user.name));
-    $header.append($('<article>').text(tweetData.user.handle));
+    $header.append($('<section>').text(tweetData.user.name));
+    $header.append($('<section>').text(tweetData.user.handle));
 
     $tweet.append($header);
   }
 });
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  $("#new-section").empty();
-    tweets.forEach(function(tweet) {
-      $('#new-section').append(createTweetElement(tweet));
-   });
-  }
+
 
 renderTweets(data);
-
-//TEST DATA
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
