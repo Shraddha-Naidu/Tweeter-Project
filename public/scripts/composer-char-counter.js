47b2500277c8145e0bf85c8fbc24a22bab2485ff
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-03-31 21:19:02 -0600

    Initial styling for new-tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-  // .attr refrences attribute
-  //.val will get text area input length
-  //link maxLength in HTML with max chars
-  $('textarea').on("input", function() {
-    var maxlength = $(this).attr("maxlength");
-    var currentLength = $(this).val().length;
+
+  $('textarea').on("keyup", function(e) {
+    //Total chars in textarea
+    let currentLength = $(this).val().length;
+    const count = 140 - currentLength;
   
     if (currentLength > maxlength) {
       return console.log("Uh Oh, too many characters and not enough space!");
