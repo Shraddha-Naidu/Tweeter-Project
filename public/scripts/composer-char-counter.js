ef864aa94430adbac84b24915b8ef10ff8d07d77
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-03-31 22:07:03 -0600

    Added Char Counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
+  // tweet input area
+  const $textArea = $('.new-tweet textarea');
 
-  $('textarea').on("keyup", function(e) {
+  $('textarea').on("keyup", function() {
     //Total chars in textarea
-    let currentLength = $(this).val().length;
-    const count = 140 - currentLength;
+    const currentLength = $(this).val().length;
+    const maxlength = 140
   
     if (currentLength > maxlength) {
       return console.log("Uh Oh, too many characters and not enough space!");
+      CountQueuingStrategy.addClass("over");
+    } else {
+      count.removeClass('over');
     }
-  
-    console.log(maxlength - currentLength);
+      console.log(maxlength - currentLength);
   });
   
 });
+
