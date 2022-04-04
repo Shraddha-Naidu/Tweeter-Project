2d44541910f081b8312ed7085230dc74df784588
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-04-04 08:04:57 -0600

    Fixed timeago and render function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,59 +1,82 @@
-/*JQuery*/
-
-$document.ready(function () {
-  //$("time.timeago").timeago();
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
   //FUNCTIONS
 
-const escape =  function(str) {
-  const div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
-
-const createTweetElement = function(tweetData) {
-  const $tweet = $('<article>').addClass('tweet');
-  const daysAgo = $.timeago.format(tweetData["created_at"]);
-
-  const innerHTMLContent = `
-    <header>
-      <img src= ${escape(tweetData.user.avatars)}>
-      <span>${escape(tweetData.user.name)}</span>
-      <span class="handle">${escape(tweetData.user.handle)}</span>
-    </header>
-    <span>${escape(tweetData.content.text)}</span>
-    <footer>
-      <span>${daysAgo} days ago</span>
-      <span>
-      <i class="fa-solid fa-circle-star"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-flag"></i>
-      </span>
-    </footer>
-  `;
-
-  return $tweet.html(innerHTMLContent);
-};
-
-//Adds all Tweets
-const renderTweets = function(beginningOfTimeTweets) {
-  for (const tweet of beginningOfTimeTweets) {
-    $('section.all-tweets').prepend(createTweetElement(tweet))
+  const escape =  function(str) {
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   }
-};
 
-const loadTweets = function () {
-  $.ajax('/tweets', {
-    method: 'GET',
-    dataType: 'JSON',
-    success: tweets => renderTweets(tweets),
-    error: (data, text, error) => console.error(error)
-  });
-};
+  const createTweetElement = function(tweetData) {
 
+    const innerHTMLContent = `
+    <article>
+      <header>
+        <img src= ${escape(tweetData.user.avatars)}>
+        <span>${escape(tweetData.user.name)}</span>
+        <span class="handle">${escape(tweetData.user.handle)}</span>
+      </header>
+      <span>${escape(tweetData.content.text)}</span>
+      <footer>
+        <span>${timeago.format(escape(tweetData.created_at))} days ago</span>
+        <span>
+        <i class="fa-solid fa-circle-star"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-flag"></i>
+        </span>
+      </footer>
+    </article>
+    `;
+  
+    return innerHTMLContent;
+  };
+  
+  //Adds all Tweets
+  const renderTweets = function(beginningOfTimeTweets) {
+    for (const tweet of beginningOfTimeTweets) {
+      $('section.all-tweets').prepend(createTweetElement(tweet))
+    }
+  };
+  
+  const loadTweets = function () {
+    $.get("/tweets/")
+      .then((data) => {
+        console.log(data);
+        renderTweets(data);
+      });
+  };
+/*JQuery*/
 
-  loadTweets();
+$(document).ready(function () {
 
+  renderTweets(data);
+
+//loadTweets();
+/* 
 $('.new-tweet form').submit(function (event) {
     event.preventDefault();
     
@@ -66,14 +89,14 @@ $('.new-tweet form').submit(function (event) {
       alert(`Uh Oh 🙃 Too many characters, please shorten!`)
    
     } else {
-      $.ajax('/tweets', {
-        data: $(this).serialize(),
-        method: 'POST',
-        success: () => {
-          loadTweets();
-          $textArea.val(''); // clear input
-          $('.counter').text('140'); // reset counter
-      }
-    })
+      $.post("/tweets/", $(this).serialize())
+        .then(() => {
+          $.get("/tweets/")
+            .then((data) => {
+              console.log(data);
+              renderTweets(data.slice(-1));
+      })
   }
+
+}); */
 });
